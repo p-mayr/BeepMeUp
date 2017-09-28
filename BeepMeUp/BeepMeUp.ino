6d0b0f18b1972d08d3fc02c2b08ba7da391f8a88
stebran <stefan-brandi@hotmail.com>
2017-09-28 09:31:12 +0200

    Existing functions organized in tabs

--- BeepMeUp/BeepMeUp.ino
+++ BeepMeUp/BeepMeUp.ino
@@ -1,32 +1,48 @@
 #include <MS5611.h>
 
+// Barometer variables
 MS5611 baro;
 float sea_pressure;
 float pressure;
 float temperature;
 float altitude;
+float vertical_speed;
+
+// Timing
+unsigned long time;
+unsigned long loop_time;
+
+// Temporary variables
+unsigned long ul_tmp;
+float f_tmp;
 
 void setup() {
   // Start barometer
   baro = MS5611();
   baro.begin();
+  sea_pressure = 1024.00;
+
+  // Set initial times
+  time = millis();
+  loop_time = time;
+  
   // Start serial (UART)
   Serial.begin(9600);
   delay(2);
-
-  sea_pressure = 1024.00;
 }
 
 void loop() {
-  altitude = lowPassFilter(altitude, getAltitude(),0.1);
-  Serial.println(altitude);
-  delay(1000);
-}
+  // Calculate loop time
+  ul_tmp = millis();
+  loop_time = ul_tmp - time;
+  time = ul_tmp;
 
-float getAltitude() {
-  pressure = (float)baro.getPressure()/100;
-  temperature = (float)baro.getTemperature()/100;
-  return ((pow((sea_pressure / pressure), 1/5.257) - 1.0) * (temperature + 273.15)) / 0.0065;
+  // Calculate vertical speed and altitude
+  getVerticalSpeed();
+
+  // Print results
+  //Serial.println(altitude);
+  Serial.println(vertical_speed);
 }
 
 float lowPassFilter(float old_value, float raw_value, float alpha){
---
+++ BeepMeUp/barometer.ino
@@ -0,0 +1,15 @@
+// Calculates the height gain resp. loss
+void getVerticalSpeed() {
+  f_tmp = getAltitude();
+  vertical_speed = lowPassFilter(vertical_speed, (f_tmp - altitude)*1000/loop_time, 0.1);
+  altitude = f_tmp;
+}
+
+
+// Calculates the altitude
+float getAltitude() {
+  pressure = (float)baro.getPressure()/100;
+  temperature = (float)baro.getTemperature()/100;
+  f_tmp = ((pow((sea_pressure / pressure), 1/5.257) - 1.0) * (temperature + 273.15)) / 0.0065;
+  return lowPassFilter(altitude, f_tmp,0.05);
+}
